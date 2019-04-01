68e137768e4dd025674dd86ffce68d3dd05d9549
DESKTOP-JB3GCLJ\Mr_Schyzo <catapano-marco@mail.com>
2019-04-01 22:16:53 +0200

    Removes useless imports

--- ClassAttempt/UIMainClasses/GameMenu.cs
+++ ClassAttempt/UIMainClasses/GameMenu.cs
@@ -8,10 +8,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Drawing;
 using System.Drawing.Drawing2D;
-using System.Drawing.Imaging;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using UIEssentials;
 
